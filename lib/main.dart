78750fc2eb4998051e9f0cb940aa4638b2c6d274
andhavarapujyothi <andhavarapujyothi>
2024-01-05 15:00:06 +0530

    flutter widgets

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo', theme: ThemeData(), home: AutocompleteScreen());
+        title: 'Flutter', theme: ThemeData(), home: AutocompleteScreen());
   }
 }
